65fd86747d6fc4be4f81f749c71b32716b5b4338
MoreiraJulia <julia.aparecida25@gmail.com>
2024-07-15 21:49:07 -0300

    Ajuste no arquivo Do while

--- estruturas-de-repeticao/src/ExemploDoWhile.java
+++ estruturas-de-repeticao/src/ExemploDoWhile.java
@@ -1,28 +1,20 @@
 import java.util.Random;
 public class ExemploDoWhile {
     public static void main(String[] args) {
-        // System.out.println("Discando...");
-        // do {
-        //     //excutando repetidas vezes até alguém atender
-        //     System.out.println("Telefone tocando");
-            
-        // }while(tocando());
-            
-        // System.out.println("Alô !!!");
-
-        int num = 5, count = 1;
+        System.out.println("Discando...");
         do {
-            num += count;
-            System.out.println(num);
-        } while (count <= 3);
-
-
+            //excutando repetidas vezes até alguém atender
+            System.out.println("Telefone tocando");
+            
+        }while(tocando());
+            
+        System.out.println("Alô !!!");
     }
-	// private static boolean tocando() {
-	// 	boolean atendeu = new Random().nextInt(3)==1;
-	// 	System.out.println("Atendeu? " + atendeu);
-	// 	//negando o ato de continuar tocando
-	// 	return ! atendeu;
-	// }
+	private static boolean tocando() {
+		boolean atendeu = new Random().nextInt(3)==1;
+		System.out.println("Atendeu? " + atendeu);
+		//negando o ato de continuar tocando
+		return ! atendeu;
+	}
 
 }
